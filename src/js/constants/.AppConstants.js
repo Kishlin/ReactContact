77accfa2a8850994aaee27d24b5bc354d1089475
Kishlin <legrand.pierrelouis@gmail.com>
2017-06-19 02:09:28 +0200

    Updated AppConstant template.

--- src/js/constants/.AppConstants.js
+++ src/js/constants/.AppConstants.js
@@ -1,6 +1,9 @@
 module.exports = {
-	SAVE_CONTACT: 'SAVE_CONTACT',
+	SAVE_CONTACT:    'SAVE_CONTACT',
 	RECEIVE_CONTACT: 'RECEIVE_CONTACT',
+	REMOVE_CONTACT:  'REMOVE_CONTACT',
+	EDIT_CONTACT:    'EDIT_CONTACT',
+	UPDATE_CONTACT:  'UPDATE_CONTACT',
 	FIREBASE: {
 	    apiKey: "#api_key#",
 	    authDomain: "#domain#.firebaseapp.com",
